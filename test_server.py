27e3400b84708d658a6700e73d77fe466d663097
ethandmd <ethandmd@protonmail.com>
2023-08-28 23:54:01 -0700

    Broken when used with flask. Works without flask

--- test_server.py
+++ test_server.py
@@ -1,40 +1,24 @@
-#import cv2 as cv
-import socket
+import cv2 as cv
 import time
 import numpy as np
 from flask import Flask, Response
 
 app = Flask(__name__)
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('', 8888))
-#fourcc = cv.VideoWriter_fourcc(*'HVID')
-#out = cv.VideoWriter('output.avi', fourcc, 15.0, (1280, 720))
-
-#count = 0
-#frames = []
+cap = cv.VideoCapture('udp://192.168.1.21:8888')
 
 def gen_frames():
-    count = 0
-    frames = []
+
     while True:
-        data, addr = sock.recvfrom(2**20)
-        #buf = np.asarray(bytearray(data), dtype=np.uint8)
-        #buf = np.frombuffer(data, dtype=np.uint8)
-        #jpg = cv.imdecode(buf, cv.IMREAD_COLOR)
-        #ret, jpg = cv.imencode('.jpg', buf)
-        #frame = jpg.tobytes()
-        #cv.imwrite('images/image_' + str(count) + '.jpg', jpg)
-        #out.write(buf)
-        #frames.append(buf)
-        #cv.imshow('frame', buf)
-        count += 1
+        ret, buff = cap.read()
+        if not ret:
+            break
+        ret, frame = cv.imencode('.jpg', buff)
+        data = frame.tobytes()
         yield(
                 b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n'
                 b'Content-Length: ' + f"{len(data)}".encode() + b'\r\n'
-                b'\r\n' + data + b'\r\n'
-        )
+                b'\r\n' + data + b'\r\n')
         time.sleep(0.01)
 
 @app.route('/feed')
@@ -46,6 +30,3 @@ def index():
     return 'image:<br><img src="/feed" />'
 
 app.run(debug=True)
-
-sock.close()
-#out.release()
