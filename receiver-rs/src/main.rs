86762fb5948106f686143de332d544159622a2f0
ethandmd <ethandmd@protonmail.com>
2023-12-27 23:34:45 -0800

    Axum + opencv gstreamer backend capture.
    Most browsers do not seem to play nice with this method so far. But this is the skeleton of streaming to the browser.

--- receiver-rs/src/main.rs
+++ receiver-rs/src/main.rs
@@ -1,21 +1,117 @@
-use opencv::{highgui, prelude::*, videoio, Result};
+use axum::{
+    body,
+    extract::connect_info::ConnectInfo,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use clap::Parser;
+use futures::stream::Stream;
+use lazy_static::lazy_static;
+use opencv::{core::Vector, imgcodecs, prelude::*, videoio};
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::{net::SocketAddr, path::PathBuf};
+use tokio::net::TcpListener;
+use tower_http::{
+    services::ServeDir,
+    trace::{DefaultMakeSpan, TraceLayer},
+};
 
-fn main() -> Result<()> {
-    highgui::named_window("video", highgui::WINDOW_FULLSCREEN)?;
-    let mut cam = videoio::VideoCapture::from_file(
-        "udpsrc port=12345 caps=application/x-rtp ! rtph264depay ! h264parse ! decodebin ! videoconvert ! appsink",
-        videoio::CAP_GSTREAMER,
-    )?;
-    let opened = videoio::VideoCapture::is_opened(&cam)?;
-    if !opened {
-        panic!("Unable to receive capture!");
-    }
-    loop {
-        let mut frame = Mat::default();
-        cam.read(&mut frame)?;
-        if frame.size()?.width > 0 {
-            highgui::imshow("video", &mut frame)?;
+#[derive(Parser)]
+struct Opts {
+    #[arg(short, long, default_value = "12345")]
+    port: u16,
+}
+
+lazy_static! {
+    static ref OPTS: Opts = Opts::parse();
+}
+
+struct StreamCapture {
+    cap: videoio::VideoCapture,
+}
+
+impl StreamCapture {
+    async fn new(port: u16) -> Result<Self, opencv::Error> {
+        let cap = videoio::VideoCapture::from_file(format!(
+                "udpsrc port={} caps=application/x-rtp ! rtph264depay ! h264parse ! decodebin ! videoconvert ! appsink",
+                port).as_str(),
+            videoio::CAP_GSTREAMER,
+        )?;
+        if !videoio::VideoCapture::is_opened(&cap)? {
+            Err(opencv::Error::new(
+                1,
+                format!("Unable to open video stream @ udp:// {}", port),
+            ))
+        } else {
+            Ok(Self { cap })
         }
-        highgui::wait_key(1)?;
     }
+}
+
+impl Stream for StreamCapture {
+    type Item = Result<std::boxed::Box<[u8]>, opencv::Error>;
+
+    fn poll_next(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut buf = Vector::new();
+        let mut frame = Mat::default();
+        self.get_mut().cap.read(&mut frame).unwrap();
+        if let Err(e) = imgcodecs::imencode(".jpg", &frame, &mut buf, &Vector::new()) {
+            println!("Error: {}", e);
+            return Poll::Ready(None);
+        }
+        let response = format!(
+            r#"
+            --frame\r\n
+            Content-Type: image/jpeg\r\n
+            Content-Length: {}\r\n
+            Connection: keep-alive\r\n
+            Cache-Control: no-cache, no-store, must-revalidate\r\n\r\n"#,
+            buf.len()
+        );
+        let mut bytes = response.into_bytes();
+        bytes.append(&mut buf.as_slice().to_vec());
+        bytes.append(&mut b"\r\n".to_vec());
+        Poll::Ready(Some(Ok(bytes.into_boxed_slice())))
+    }
+}
+
+// TODO: unwrap() on builder.
+async fn feed(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> impl IntoResponse {
+    println!("User: {addr} connected.");
+    if let Ok(stream) = StreamCapture::new(OPTS.port).await {
+        Response::builder()
+            .status(200)
+            .header("Content-Type", "multipart/x-mixed-replace; boundary=frame")
+            .body(body::Body::from_stream(stream))
+            .unwrap()
+        //body::Body::from_stream(stream)
+    } else {
+        Response::builder()
+            .status(500)
+            .body(body::Body::from("Unable to open video stream."))
+            .unwrap()
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let assets = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
+    let app = Router::new()
+        .fallback_service(ServeDir::new(assets).append_index_html_on_directories(true))
+        .route("/feed", get(feed))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::default().include_headers(true)),
+        );
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap();
 }
